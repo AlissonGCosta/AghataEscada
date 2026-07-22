ea4ba4f38b6f719b61a69ba2e511c3bec1fec7bf
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 00:01:54 -0300

    Initial commit

---
+++ src/main/java/br/costa/AghataEscada/AghataEscadaApplication.java
@@ -0,0 +1,13 @@
+package br.costa.AghataEscada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AghataEscadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AghataEscadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/costa/AghataEscada/AghataEscadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.costa.AghataEscada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AghataEscadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
