82d77a12ab9516f7859575014e5b24952cc6694a
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 23:40:56 -0300

    feat(employee): adiciona criador de funcionarios

---
+++ src/main/java/br/costa/AghataEscada/epmloyeer/service/create/EmployeeCreate.java
@@ -0,0 +1,15 @@
+package br.costa.AghataEscada.epmloyeer.service.create;
+
+import br.costa.AghataEscada.epmloyeer.entity.EmployeeEntity;
+import br.costa.AghataEscada.epmloyeer.entity.dto.request.EmployeeRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeCreate {
+
+    public EmployeeEntity createEmployee(EmployeeRequestDto dto) {
+
+
+
+    }
+}
