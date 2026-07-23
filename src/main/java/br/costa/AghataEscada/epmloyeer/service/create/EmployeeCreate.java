6961ad564e1ccc9b25e0ae14f57a04bf28d0c77b
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 23:52:20 -0300

    feat(employee): implementa criacao de funcionario

--- src/main/java/br/costa/AghataEscada/epmloyeer/service/create/EmployeeCreate.java
+++ src/main/java/br/costa/AghataEscada/epmloyeer/service/create/EmployeeCreate.java
@@ -1,15 +1,32 @@
 package br.costa.AghataEscada.epmloyeer.service.create;
 
+import br.costa.AghataEscada.config.PasswordConifg;
 import br.costa.AghataEscada.epmloyeer.entity.EmployeeEntity;
 import br.costa.AghataEscada.epmloyeer.entity.dto.request.EmployeeRequestDto;
+import br.costa.AghataEscada.epmloyeer.entity.enumemployee.EmployeeEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
+@RequiredArgsConstructor
 public class EmployeeCreate {
+
+    private final PasswordConifg conifg;
 
     public EmployeeEntity createEmployee(EmployeeRequestDto dto) {
 
+        EmployeeEntity employee = new EmployeeEntity();
+        employee.setName(dto.name());
+        employee.setPosition(dto.position());
+        employee.setCltNumber(dto.cltNumber());
+        employee.setPassword(conifg.passwordEncoder().encode(dto.password()));
+        employee.setEnumEmployee(EmployeeEnum.ROLE_USER);
+        employee.setCreatedAT(LocalDate.now());
+        employee.setUpdatedAT(LocalDate.now());
 
 
+        return employee;
     }
 }
