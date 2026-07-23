1631dfc83ae994dafb62d7a953c3674d176bab96
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 23:40:56 -0300

    feat(employee): adiciona validador de funcionarios

---
+++ src/main/java/br/costa/AghataEscada/epmloyeer/service/validate/EmployeeValidateService.java
@@ -0,0 +1,12 @@
+package br.costa.AghataEscada.epmloyeer.service.validate;
+
+import br.costa.AghataEscada.epmloyeer.entity.dto.request.EmployeeRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeValidateService {
+
+    public void validateEmployee(EmployeeRequestDto dto) {
+
+    }
+}
