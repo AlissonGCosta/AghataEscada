b0e70173936f20dd2e6534934721e85dbb7b2bcc
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 23:40:58 -0300

    feat(app): configura cliente Feign

--- src/main/java/br/costa/AghataEscada/AghataEscadaApplication.java
+++ src/main/java/br/costa/AghataEscada/AghataEscadaApplication.java
@@ -2,8 +2,10 @@ package br.costa.AghataEscada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient
 public class AghataEscadaApplication {
 
 	public static void main(String[] args) {
