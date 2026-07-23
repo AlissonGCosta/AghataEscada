1471162eb2a63aef249f85993be02716520d9184
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-22 23:40:53 -0300

    feat(employee): adiciona DTO de resposta

---
+++ src/main/java/br/costa/AghataEscada/epmloyeer/entity/dto/response/EmployeeResponseDto.java
@@ -0,0 +1,12 @@
+package br.costa.AghataEscada.epmloyeer.entity.dto.response;
+
+public record EmployeeResponseDto(
+        String name,
+        String position,
+        String cltNumber,
+        String sector,
+        String password
+
+) {
+
+}
