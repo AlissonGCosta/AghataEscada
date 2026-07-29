cd53da0d1cf460478db44c05e9ad0302402ff1ef
AlissonGCosta <alisson.costa84200@gmail.com>
2026-07-28 23:41:53 -0300

    feat(productstorage): add ProductStorageEntity JPA model

---
+++ src/main/java/br/costa/AghataEscada/productstorage/entity/ProductStorageEntity.java
@@ -0,0 +1,48 @@
+package br.costa.AghataEscada.productstorage.entity;
+
+import br.costa.AghataEscada.epmloyeer.entity.EmployeeEntity;
+import br.costa.AghataEscada.productstorage.entity.productenum.ProductStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "product_storage")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductStorageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String part;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private ProductStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "id_employee", nullable = false)
+    private EmployeeEntity employer;
+
+
+}
