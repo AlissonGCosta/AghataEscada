19e77ba629e6ab4e13a0377ba863b095b205e3f1
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-01 23:52:48 -0300

    feat(managerstorage): add storage manager entity

---
+++ src/main/java/br/costa/AghataEscada/managerstorage/entity/ManagerStorageEntity.java
@@ -0,0 +1,42 @@
+package br.costa.AghataEscada.managerstorage.entity;
+
+import br.costa.AghataEscada.managerstorage.entity.storageenum.ProductStorageManagerEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Table(name = "storage_manager")
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ManagerStorageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "product_name", nullable = false)
+    private String nameProduct;
+
+    @Column(name = "product_quantity", nullable = false)
+    private Integer productQuantity;
+
+    @Column(name = "product_part", nullable = false)
+    private String productPart;
+
+    @Column(nullable = false)
+    private ProductStorageManagerEnum status;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+
+
+}
