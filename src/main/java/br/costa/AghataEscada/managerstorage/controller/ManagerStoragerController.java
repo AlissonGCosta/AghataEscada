cf00e743c830331dc50aca2e5211eedfe745d03c
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-02 23:36:34 -0300

    feat(manager-storage): expose product listing endpoint

---
+++ src/main/java/br/costa/AghataEscada/managerstorage/controller/ManagerStoragerController.java
@@ -0,0 +1,28 @@
+package br.costa.AghataEscada.managerstorage.controller;
+
+import br.costa.AghataEscada.managerstorage.dto.response.ManagerStorageResponseDto;
+import br.costa.AghataEscada.managerstorage.service.ProductStorageManagerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/manager")
+@RequiredArgsConstructor
+public class ManagerStoragerController {
+
+    private final ProductStorageManagerService managerService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ManagerStorageResponseDto> findAllProductusManager(){
+
+        return managerService.findAllProductsManager();
+    }
+
+}
