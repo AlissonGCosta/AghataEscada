de66ddac8e78e96f5e3a92459d77d00044025787
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-02 23:36:33 -0300

    feat(manager-storage): synchronize and list products

--- src/main/java/br/costa/AghataEscada/managerstorage/service/ProductStorageManagerService.java
+++ src/main/java/br/costa/AghataEscada/managerstorage/service/ProductStorageManagerService.java
@@ -1,9 +1,17 @@
 package br.costa.AghataEscada.managerstorage.service;
 
+import br.costa.AghataEscada.managerstorage.dto.response.ManagerStorageResponseDto;
+import br.costa.AghataEscada.managerstorage.entity.ManagerStorageEntity;
+import br.costa.AghataEscada.managerstorage.entity.storageenum.ProductStorageManagerEnum;
 import br.costa.AghataEscada.managerstorage.repository.ProductStorageManagerRepository;
+import br.costa.AghataEscada.productstorage.entity.ProductStorageEntity;
 import br.costa.AghataEscada.productstorage.entity.dto.request.ProductStorageRequestDto;
+import br.costa.AghataEscada.productstorage.repository.ProductStorageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +20,45 @@ public class ProductStorageManagerService {
     // calling the repository
     private final ProductStorageManagerRepository managerRepository;
 
-    public void createProductStorageManager(ProductStorageRequestDto dto){
+   //third repository
+    private final ProductStorageRepository productStorageRepository;
 
+    public void attManagerTable(){
+
+   List<ProductStorageEntity> products =  productStorageRepository.findAll();
+
+   List<ManagerStorageEntity> managerProducts = products.stream()
+           .map(product -> ManagerStorageEntity.builder()
+                   .nameProduct(product.getName())
+                   .productPart(product.getPart())
+                   .productQuantity((product.getQuantity()))
+                   .status(ProductStorageManagerEnum.UNUSABLE)
+                   .createdAt(LocalDate.now())
+                   .updatedAt(LocalDate.now())
+                   .build()
+
+           )
+           .toList();
+
+
+
+    managerRepository.saveAll(managerProducts);
     }
+
+
+    public List<ManagerStorageResponseDto> findAllProductsManager(){
+
+        return managerRepository.findAll().stream()
+                .map(pt -> new ManagerStorageResponseDto(
+                        pt.getId(),
+                        pt.getNameProduct(),
+                        pt.getProductQuantity(),
+                        pt.getProductPart(),
+                        pt.getStatus(),
+                        pt.getCreatedAt(),
+                        pt.getUpdatedAt()
+                ))
+                .toList();
+    }
+
 }
