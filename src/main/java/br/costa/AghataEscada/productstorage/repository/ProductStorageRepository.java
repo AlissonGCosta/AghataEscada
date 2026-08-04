be8d9606704c9e1d00458279000ca5859a7304b2
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-03 23:56:17 -0300

    feat(product-storage): add aggregated product queries

--- src/main/java/br/costa/AghataEscada/productstorage/repository/ProductStorageRepository.java
+++ src/main/java/br/costa/AghataEscada/productstorage/repository/ProductStorageRepository.java
@@ -1,13 +1,26 @@
 package br.costa.AghataEscada.productstorage.repository;
 
+import br.costa.AghataEscada.managerstorage.repository.ProductSummary;
 import br.costa.AghataEscada.productstorage.entity.ProductStorageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductStorageRepository extends JpaRepository<ProductStorageEntity, UUID> {
-    Optional<ProductStorageEntity> findByPart(String part);
+    List<ProductStorageEntity> findByPart(String part);
+    List<ProductStorageEntity> findByName(String name);
     List<ProductStorageEntity> findAllByNameContainingIgnoreCase(String name);
+
+    @Query("""
+            SELECT
+             product.name AS name,
+             product.part AS part,
+             SUM(product.quantity) AS quantity
+             FROM ProductStorageEntity product
+             GROUP BY product.name, product.part
+""")
+    List<ProductSummary> findProductSummary();
 }
