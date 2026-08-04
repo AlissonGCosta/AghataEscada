50edb56360e1ec659cdb264c94dd9e67ff1a4cd8
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-03 23:56:18 -0300

    feat(product-storage): validate product name conflicts

--- src/main/java/br/costa/AghataEscada/productstorage/service/ValidateProduct.java
+++ src/main/java/br/costa/AghataEscada/productstorage/service/ValidateProduct.java
@@ -1,14 +1,32 @@
 package br.costa.AghataEscada.productstorage.service;
 
 import br.costa.AghataEscada.exception.errorcase.BusinessException;
+import br.costa.AghataEscada.exception.errorcase.ConflictException;
+import br.costa.AghataEscada.exception.errorcase.RessourceNotFoundException;
+import br.costa.AghataEscada.productstorage.entity.ProductStorageEntity;
 import br.costa.AghataEscada.productstorage.entity.dto.request.ProductStorageRequestDto;
+import br.costa.AghataEscada.productstorage.repository.ProductStorageRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ValidateProduct {
+
+    // calling repository
+   private final ProductStorageRepository productStorageRepository;
 
     public void validate(ProductStorageRequestDto dto){
 
+
+
+        if(!productStorageRepository.findByPart(dto.part()).isEmpty()){
+
+            if(productStorageRepository.findByName(dto.name()).isEmpty()){
+                throw new ConflictException("incorrect name");
+            }
+
+        }
 
         if(dto.quantity() < 0){
             throw new BusinessException("Invalid quantity");
