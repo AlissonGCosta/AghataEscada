0b4eb3b4ea168294a1d28eadd2a381a4bb8bba5a
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-04 23:52:56 -0300

    feat(security): allow manager read and update routes

--- src/main/java/br/costa/AghataEscada/config/SecurityConfig.java
+++ src/main/java/br/costa/AghataEscada/config/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/v1/product/{id}").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/v1/product/{empId}/product/{prodId}").permitAll()
                         .requestMatchers(HttpMethod.PATCH, "/v1/product/{empId}/product/{prodId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/manager").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/manager/{id}").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/v1/manager/{id}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers
