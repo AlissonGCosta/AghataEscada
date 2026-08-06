8bab2f09bd92455ba449a0630e2909c44bbfab84
AlissonGCosta <alisson.costa84200@gmail.com>
2026-08-05 23:35:05 -0300

    feat(manager-storage): add product name lookup query

--- src/main/java/br/costa/AghataEscada/managerstorage/repository/ProductStorageManagerRepository.java
+++ src/main/java/br/costa/AghataEscada/managerstorage/repository/ProductStorageManagerRepository.java
@@ -4,7 +4,10 @@ package br.costa.AghataEscada.managerstorage.repository;
 import br.costa.AghataEscada.managerstorage.entity.ManagerStorageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductStorageManagerRepository extends JpaRepository<ManagerStorageEntity, UUID> {
+
+    List<ManagerStorageEntity> findManagerStorageByNameProduct(String nameProduct);
 }
